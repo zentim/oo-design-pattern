88a9e70a7111a92ef88f4f75b21bb4549eb41682
zentim <zentim3248@gmail.com>
2018-11-21 22:28:10 +0800

    add PrototypeMgr

--- Prototype.java
+++ Prototype.java
@@ -1,16 +1,46 @@
 /**
- * Prototype
+ * PrototypeMgr (Use Singleton Pattern)
  */
-public class Item implements Cloneable {
-  private String name;
+public class PrototypeMgr() {
+  private static HashTable<String, Prototype> prototypes = new HashTable<String, Prototype>();
 
-  public Item(String name) {
-    this.name = name;
+  private PrototypeMgr(){}
+
+  public static Prototype getPrototype(String name) {
+    Prototype originalPrototype = prototypes.get(name);
+    return (Prototype) originalPrototype.clone();
   }
 
+  public static init() {
+    ConcretePrototype originalPrototype = new ConcretePrototype();
+    originalPrototype.setName("originalPrototype");
+    prototypes.put(originalPrototype.getName(), originalPrototype);
+
+    System.out.println(originalPrototype.getName()); // originalPrototype
+  }
+
+}
+
+/**
+ * Prototype
+ */
+public class Prototype implements Cloneable {
+  private String name;
+  private String type;
+
+  public abstract void doSomething();
+
   @Override
-  public Object clone() throws CloneNotSupportedException {
-    return new Item(name);
+  public Object clone() {
+    Object clone = null;
+
+    try {
+      clone = super.clone();
+    } catch (CloneNotSupportedException) {
+      e.printStackTrace();
+    }
+
+    return clone;
   }
 
   /* Getter and Setter */
@@ -20,6 +50,23 @@ public class Item implements Cloneable {
   public void setName(String name) {
     this.name = name;
   }
+  public String getType() {
+    return type;
+  }
+}
+
+/**
+ * ConcretePrototype
+ */
+public class ConcretePrototype extends Prototype {
+  public ConcretePrototype() {
+    type = "ConcretePrototype";
+  }
+
+  @Override
+  public void doSomething() {
+    System.out.println("ConcretePrototype doSomething");
+  }
 }
 
 /**
@@ -27,11 +74,13 @@ public class Item implements Cloneable {
  */
 public class Client {
   public static void main(String[] args) {
-    private Item original = new Item("Item1");
-    private Item clone = original.clone();
-    clone.setName("Item2");
+    PrototypeMgr.init(); // init originalPrototype
+    private Prototype cloned1 = PrototypeMgr.getPrototype("originalPrototype");
+    private Prototype cloned2 = PrototypeMgr.getPrototype("originalPrototype");
+    cloned1.setName("clonedPrototype1");
+    cloned2.setName("clonedPrototype2");
 
-    System.out.println(original.getName()); // Item1
-    System.out.println(clone.getName()); // Item2
+    System.out.println(cloned1.getName()); // clonedPrototype1
+    System.out.println(cloned2.getName()); // clonedPrototype2
   }
 }
